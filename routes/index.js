7469daf93c2d0bde430675a93213f4f580182f26
cnguyen123 <cnguyen111085@gmail.com>
2026-06-29 13:45:24 +0200

    Initial node-hostname Kubernetes deployment

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var os = require('os');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({
+    message: "Testing for rolling update node-hostname - Chanh Nguyen, 29 June 2026",
     hostname: os.hostname(),
     version: process.env.npm_package_version,
   });
